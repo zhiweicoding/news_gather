da87d084a69826384ed8162d863b34f13f858ac1
zhiweicoding <diaozhiwei2k@163.com>
2024-05-12 22:04:34 +0800

    github action

---
+++ json_parse_test.py
@@ -0,0 +1,403 @@
+import json
+
+json_content='''
+{
+  "entryId": "tweet-1789291527472378194",
+  "sortIndex": "1789329497686802432",
+  "content": {
+    "entryType": "TimelineTimelineItem",
+    "__typename": "TimelineTimelineItem",
+    "itemContent": {
+      "itemType": "TimelineTweet",
+      "__typename": "TimelineTweet",
+      "tweet_results": {
+        "result": {
+          "__typename": "Tweet",
+          "rest_id": "1789291527472378194",
+          "core": {
+            "user_results": {
+              "result": {
+                "__typename": "User",
+                "id": "VXNlcjoxNzY0ODE4MzMxODIyMTgyNDAw",
+                "rest_id": "1764818331822182400",
+                "affiliates_highlighted_label": {},
+                "has_graduated_access": true,
+                "is_blue_verified": true,
+                "profile_image_shape": "Circle",
+                "legacy": {
+                  "following": true,
+                  "can_dm": true,
+                  "can_media_tag": true,
+                  "created_at": "Tue Mar 05 01:01:23 +0000 2024",
+                  "default_profile": true,
+                  "default_profile_image": false,
+                  "description": "Weibo 528w 超高过气科技博主、ChatGPT安装工程师、神经网络导航员、大自然语言模型搬运工、数据海洋大力水手、WaytoAGI之鹿饲养员\n\nhttps://t.co/7dEiCJ7Us5 首席编撰官兼客服",
+                  "entities": {
+                    "description": {
+                      "urls": [
+                        {
+                          "display_url": "XiaoHu.AI",
+                          "expanded_url": "http://XiaoHu.AI",
+                          "url": "https://t.co/7dEiCJ7Us5",
+                          "indices": [
+                            76,
+                            99
+                          ]
+                        }
+                      ]
+                    },
+                    "url": {
+                      "urls": [
+                        {
+                          "display_url": "xiaohu.ai",
+                          "expanded_url": "http://xiaohu.ai",
+                          "url": "https://t.co/4PVaHEr5r3",
+                          "indices": [
+                            0,
+                            23
+                          ]
+                        }
+                      ]
+                    }
+                  },
+                  "fast_followers_count": 0,
+                  "favourites_count": 392,
+                  "followers_count": 19917,
+                  "friends_count": 798,
+                  "has_custom_timelines": false,
+                  "is_translator": false,
+                  "listed_count": 243,
+                  "location": "Mars",
+                  "media_count": 602,
+                  "name": "小互",
+                  "normal_followers_count": 19917,
+                  "pinned_tweet_ids_str": [],
+                  "possibly_sensitive": false,
+                  "profile_image_url_https": "https://pbs.twimg.com/profile_images/1765404718959095808/BX7VN1hS_normal.jpg",
+                  "profile_interstitial_type": "",
+                  "screen_name": "imxiaohu",
+                  "statuses_count": 977,
+                  "translator_type": "none",
+                  "url": "https://t.co/4PVaHEr5r3",
+                  "verified": false,
+                  "want_retweets": true,
+                  "withheld_in_countries": []
+                },
+                "tipjar_settings": {}
+              }
+            }
+          },
+          "unmention_data": {},
+          "edit_control": {
+            "edit_tweet_ids": [
+              "1789291527472378194"
+            ],
+            "editable_until_msecs": "1715438918000",
+            "is_edit_eligible": true,
+            "edits_remaining": "5"
+          },
+          "is_translatable": true,
+          "views": {
+            "count": "3617",
+            "state": "EnabledWithCount"
+          },
+          "source": "\u003ca href\u003d\"https://mobile.twitter.com\" rel\u003d\"nofollow\"\u003eTwitter Web App\u003c/a\u003e",
+          "note_tweet": {
+            "is_expandable": true,
+            "note_tweet_results": {
+              "result": {
+                "id": "Tm90ZVR3ZWV0OjE3ODkyOTE1MjczMzM5NjE3Mjg\u003d",
+                "text": "Llama3 中文综合资源库 \n\n集合了与Llama3 模型相关的各种中文资料\n\n该仓库提供了多个版本的Llama3 模型，包括基于不同技术和偏好的微调版本，如直接中文SFT版、Instruct偏好强化学习版、趣味版等。\n\n还包括微调版本、有趣的权重、训练、推理、评测和部署的教程视频与文档。\n\n详细内容：https://t.co/e8eBhhfdC5",
+                "entity_set": {
+                  "hashtags": [],
+                  "symbols": [],
+                  "urls": [
+                    {
+                      "display_url": "xiaohu.ai/p/7766",
+                      "expanded_url": "https://xiaohu.ai/p/7766",
+                      "url": "https://t.co/e8eBhhfdC5",
+                      "indices": [
+                        153,
+                        176
+                      ]
+                    }
+                  ],
+                  "user_mentions": []
+                },
+                "richtext": {
+                  "richtext_tags": []
+                },
+                "media": {
+                  "inline_media": []
+                }
+              }
+            }
+          },
+          "legacy": {
+            "bookmark_count": 31,
+            "bookmarked": false,
+            "created_at": "Sat May 11 13:48:38 +0000 2024",
+            "conversation_id_str": "1789291527472378194",
+            "display_text_range": [
+              0,
+              147
+            ],
+            "entities": {
+              "hashtags": [],
+              "media": [
+                {
+                  "display_url": "pic.x.com/skmxbx5ooy",
+                  "expanded_url": "https://twitter.com/imxiaohu/status/1789291527472378194/photo/1",
+                  "id_str": "1789291452801200129",
+                  "indices": [
+                    148,
+                    171
+                  ],
+                  "media_key": "3_1789291452801200129",
+                  "media_url_https": "https://pbs.twimg.com/media/GNTXGKPbsAET995.png",
+                  "type": "photo",
+                  "url": "https://t.co/SKmXbX5ooy",
+                  "ext_media_availability": {
+                    "status": "Available"
+                  },
+                  "features": {
+                    "large": {
+                      "faces": []
+                    },
+                    "medium": {
+                      "faces": []
+                    },
+                    "small": {
+                      "faces": []
+                    },
+                    "orig": {
+                      "faces": []
+                    }
+                  },
+                  "sizes": {
+                    "large": {
+                      "h": 862,
+                      "w": 778,
+                      "resize": "fit"
+                    },
+                    "medium": {
+                      "h": 862,
+                      "w": 778,
+                      "resize": "fit"
+                    },
+                    "small": {
+                      "h": 680,
+                      "w": 614,
+                      "resize": "fit"
+                    },
+                    "thumb": {
+                      "h": 150,
+                      "w": 150,
+                      "resize": "crop"
+                    }
+                  },
+                  "original_info": {
+                    "height": 862,
+                    "width": 778,
+                    "focus_rects": [
+                      {
+                        "x": 0,
+                        "y": 0,
+                        "w": 778,
+                        "h": 436
+                      },
+                      {
+                        "x": 0,
+                        "y": 0,
+                        "w": 778,
+                        "h": 778
+                      },
+                      {
+                        "x": 0,
+                        "y": 0,
+                        "w": 756,
+                        "h": 862
+                      },
+                      {
+                        "x": 0,
+                        "y": 0,
+                        "w": 431,
+                        "h": 862
+                      },
+                      {
+                        "x": 0,
+                        "y": 0,
+                        "w": 778,
+                        "h": 862
+                      }
+                    ]
+                  },
+                  "allow_download_status": {
+                    "allow_download": true
+                  },
+                  "media_results": {
+                    "result": {
+                      "media_key": "3_1789291452801200129"
+                    }
+                  }
+                }
+              ],
+              "symbols": [],
+              "timestamps": [],
+              "urls": [],
+              "user_mentions": []
+            },
+            "extended_entities": {
+              "media": [
+                {
+                  "display_url": "pic.twitter.com/SKmXbX5ooy",
+                  "expanded_url": "https://twitter.com/imxiaohu/status/1789291527472378194/photo/1",
+                  "id_str": "1789291452801200129",
+                  "indices": [
+                    148,
+                    171
+                  ],
+                  "media_key": "3_1789291452801200129",
+                  "media_url_https": "https://pbs.twimg.com/media/GNTXGKPbsAET995.png",
+                  "type": "photo",
+                  "url": "https://t.co/SKmXbX5ooy",
+                  "ext_media_availability": {
+                    "status": "Available"
+                  },
+                  "features": {
+                    "large": {
+                      "faces": []
+                    },
+                    "medium": {
+                      "faces": []
+                    },
+                    "small": {
+                      "faces": []
+                    },
+                    "orig": {
+                      "faces": []
+                    }
+                  },
+                  "sizes": {
+                    "large": {
+                      "h": 862,
+                      "w": 778,
+                      "resize": "fit"
+                    },
+                    "medium": {
+                      "h": 862,
+                      "w": 778,
+                      "resize": "fit"
+                    },
+                    "small": {
+                      "h": 680,
+                      "w": 614,
+                      "resize": "fit"
+                    },
+                    "thumb": {
+                      "h": 150,
+                      "w": 150,
+                      "resize": "crop"
+                    }
+                  },
+                  "original_info": {
+                    "height": 862,
+                    "width": 778,
+                    "focus_rects": [
+                      {
+                        "x": 0,
+                        "y": 0,
+                        "w": 778,
+                        "h": 436
+                      },
+                      {
+                        "x": 0,
+                        "y": 0,
+                        "w": 778,
+                        "h": 778
+                      },
+                      {
+                        "x": 0,
+                        "y": 0,
+                        "w": 756,
+                        "h": 862
+                      },
+                      {
+                        "x": 0,
+                        "y": 0,
+                        "w": 431,
+                        "h": 862
+                      },
+                      {
+                        "x": 0,
+                        "y": 0,
+                        "w": 778,
+                        "h": 862
+                      }
+                    ]
+                  },
+                  "allow_download_status": {
+                    "allow_download": true
+                  },
+                  "media_results": {
+                    "result": {
+                      "media_key": "3_1789291452801200129"
+                    }
+                  }
+                }
+              ]
+            },
+            "favorite_count": 40,
+            "favorited": false,
+            "full_text": "Llama3 中文综合资源库 \n\n集合了与Llama3 模型相关的各种中文资料\n\n该仓库提供了多个版本的Llama3 模型，包括基于不同技术和偏好的微调版本，如直接中文SFT版、Instruct偏好强化学习版、趣味版等。\n\n还包括微调版本、有趣的权重、训练、推理、评测和部署的教程视频与文档。… https://t.co/SKmXbX5ooy",
+            "is_quote_status": false,
+            "lang": "zh",
+            "possibly_sensitive": false,
+            "possibly_sensitive_editable": true,
+            "quote_count": 0,
+            "reply_count": 1,
+            "retweet_count": 8,
+            "retweeted": false,
+            "user_id_str": "1764818331822182400",
+            "id_str": "1789291527472378194"
+          },
+          "quick_promote_eligibility": {
+            "eligibility": "IneligibleNotProfessional"
+          }
+        }
+      },
+      "tweetDisplayType": "Tweet"
+    },
+    "clientEventInfo": {
+      "component": "tweet",
+      "element": "tweet",
+      "details": {
+        "timelinesDetails": {
+          "injectionType": "RankedOrganicTweet",
+          "controllerData": "DAACDAABDAABCgABAAAAAAAAAAAKAAkXLW66glqAAAAAAAA\u003d"
+        }
+      }
+    }
+  }
+}
+'''
+
+try:
+    # 将 JSON 字符串解析为字典
+    data = json.loads(json_content,strict=False)
+    # Ensure that we are accessing a dictionary
+    if isinstance(data, dict):
+        media_items = data.get('content', {}).get('itemContent', {}).get('tweet_results', {}).get('result', {}).get(
+            'legacy', {}).get('extended_entities', {}).get('media', [])
+
+        if isinstance(media_items, list):
+            photo_urls = [item['media_url_https'] for item in media_items if item['type'] == 'photo']
+            print("Photo URLs found:", photo_urls)
+        else:
+            print("Expected a list for media items, but found:", type(media_items).__name__)
+    else:
+        print("Data is not a dictionary")
+except KeyError as e:
+    print(f"Key {e} not found in JSON data")
+except TypeError as e:
+    print(f"Type error: {e} - Check the structure of the JSON data")
--- readMe.md
+++ readMe.md
@@ -1,2 +1,26 @@
 # news load 
 ## 1. 部署方式
+
+```shell
+docker build -t loveqianqian/news_gather:main .
+```
+
+```shell
+docker run -d -p 9080:9080 loveqianqian/news_gather:main
+```
+
+docker-compose.yml
+```docker
+version: '3.8'
+
+services:
+  fastapi-app:
+    image: loveqianqian/news_gather:main
+    container_name: new_gather
+    ports:
+      - "9080:9080"
+    environment:
+      - MODULE_NAME=main 
+      - VARIABLE_NAME=app
+    restart: always  
+```
