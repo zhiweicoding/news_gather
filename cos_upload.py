d27c5a2bcd9d9320a6d238ad792bc11bfd0af058
zhiweicoding <diaozhiwei2k@163.com>
2024-05-12 21:08:59 +0800

    init

--- cos_upload.py
+++ cos_upload.py
@@ -13,11 +13,12 @@ logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 # BucketName-Appid 组成
 secret_id = os.environ['COS_SECRET_ID']
 secret_key = os.environ['COS_SECRET_KEY']   # 用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
-region = 'ap-beijing'      # 替换为用户的 region，已创建桶归属的 region 可以在控制台查看，https://console.cloud.tencent.com/cos5/bucket
+region = None
                            # COS 支持的所有 region 列表参见 https://cloud.tencent.com/document/product/436/6224
 token = None               # 如果使用永久密钥不需要填入 token，如果使用临时密钥需要填入，临时密钥生成和使用指引参见 https://cloud.tencent.com/document/product/436/14048
 scheme = 'https'           # 指定使用 http/https 协议来访问 COS，默认为 https，可不填
 
+endpoint = 'cos.accelerate.myqcloud.com'
 config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key, Token=token, Scheme=scheme)
 client = CosS3Client(config)
 
